f979f3aad455c29f90fb6af9c40dd05dbb22168e
unknown <alexandru.pop@epitech.eu>
2023-04-28 13:59:19 +0200

    Rajout de la class vue

--- Model/Joueurs.py
+++ Model/Joueurs.py
@@ -1,16 +1,27 @@
 import os
 import json
+import os
 
 
 class Joueurs:
+    """Class Joueurs, Methodes principales: Ajouter un nouveau joueur, Récupérer les informations d'un joueur, Mettre à jour les informations d'un joueur"""
+
     # Obtention du chemin relatif vers le répertoire "Data"
     data_folder = os.path.abspath(
         os.path.join(os.path.dirname(__file__), os.pardir, "Data")
     )
     file_path = os.path.join(data_folder, "Joueurs.json")
 
+    def __init__(self, first_name, last_name, birth_date, id):
+        """Initialisation des attributs de l'objet Joueurs"""
+        self.first_name = first_name
+        self.last_name = last_name
+        self.birth_date = birth_date
+        self.id = id
+
     @classmethod
     def _get_json_file(cls):
+        """Recuperation du contenu du fichier json"""
         # Vérification si le fichier n'est pas vide
         if os.path.getsize(cls.file_path) > 0:
             with open(cls.file_path, "r") as json_data:
@@ -21,18 +32,12 @@ class Joueurs:
 
     @classmethod
     def _write_json_file(cls, data):
+        """Ecrire les donnees dans le fichier JSON"""
         with open(cls.file_path, "w", encoding="utf-8") as json_file:
             json.dump(data, json_file, ensure_ascii=False, indent=4)
 
-    def __init__(self, first_name, last_name, birth_date, id):
-        # Initialisation des attributs de l'objet Joueurs
-        self.first_name = first_name
-        self.last_name = last_name
-        self.birth_date = birth_date
-        self.id = id
-
     def add_player(self):
-        """Ajouter un nouveau joueur"""
+        """Ajouter un nouveau joueur ou returne une expetion ValueError"""
         joueur = {
             "Nom": self.last_name,
             "Prenom": self.first_name,
@@ -44,7 +49,7 @@ class Joueurs:
 
         # Vérification des doublons
         if any(el.get("ID") == joueur["ID"] for el in data):
-            print("ID déjà existant")
+            raise ValueError("Un joueur avec cet ID existe déjà.")
         else:
             data.append(joueur)
 
@@ -52,15 +57,17 @@ class Joueurs:
         Joueurs._write_json_file(data)
 
     def get_player(self, id):
-        """Récupérer les informations d'un joueur"""
+        """Récupérer les informations d'un joueur, returne le joueur ou KeyError exception"""
         data = Joueurs._get_json_file()
         for el in data:
             # Parcourir la liste des éléments et vérifier si l'ID correspond
             if el["ID"] == id:
                 return el
+            else:
+                raise KeyError("Ce Joueur n'est pas enregistré")
 
     def update_player(self, id, player_data):
-        """Mettre à jour les informations d'un joueur"""
+        """Mettre à jour les informations d'un joueur ou returne une exception KeyError"""
         data = Joueurs._get_json_file()
 
         for i, element in enumerate(data):
@@ -71,6 +78,8 @@ class Joueurs:
                 data.append(player_data)
                 # Ajouter l'élément mis à jour
                 break
+            else:
+                raise KeyError("Ce Joueur n'est pas enregistré")
 
         # Écrire les données dans le fichier JSON
         Joueurs._write_json_file(data)
@@ -83,7 +92,6 @@ update_inf = {
     "Nom": "Alexandru",
     "Prenom": "Pop",
     "Date naissance": "08/11/1991",
-    "Turnois gagnee": 1,
     "ID": "AB5454a",
 }
-player.update_player("AB5454a", update_inf)
+player.add_player()
--- Model/Tournois.py
+++ Model/Tournois.py
@@ -4,25 +4,40 @@ import re
 
 
 class Tournois:
+    """Class Tournois, Methodes principales: Creer un tournois, Récupérer les informations d'un tournois, Mettre à jour les informations d'un tournois"""
+
     # Obtention du chemin relatif vers le répertoire "Data"
     data_folder = os.path.abspath(
-        os.path.join(os.path.dirname(__file__), os.pardir, "Data")
+        os.path.join(os.path.dirname(__file__), os.pardir, "Data/Tournois")
     )
-    file_path = os.path.join(data_folder, "Tournois.json")
 
     @classmethod
-    def _get_json_file(cls):
+    def _create_json_file(cls, name, data):
+        file_path = os.path.join(cls.data_folder, name + ".json")
+        # Verifier si un turnois existe deja
+        if os.path.exists(file_path):
+            raise ValueError("Un tournois existe deja avec ce nom")
+        # Creer le fichier et ecrire les donnees
+        with open(file_path, "w", encoding="utf-8") as json_file:
+            json.dump(data, json_file, ensure_ascii=False, indent=4)
+
+    @classmethod
+    def _get_json_file(cls, file_name):
+        """Recuperation du contenu du fichier json"""
+        file_path = os.path.join(cls.data_folder, file_name + ".json")
         # Vérification si le fichier n'est pas vide
-        if os.path.getsize(cls.file_path) > 0:
-            with open(cls.file_path, "r", encoding="utf-8") as json_data:
+        if os.path.getsize(file_path) > 0:
+            with open(file_path, "r", encoding="utf-8") as json_data:
                 # Charger les données existantes
                 return json.load(json_data)
         else:
             return []
 
     @classmethod
-    def _write_json_file(cls, data):
-        with open(cls.file_path, "w", encoding="utf-8") as json_file:
+    def _write_json_file(cls, file_name, data):
+        """Ecrire les donnees dans le fichier JSON"""
+        file_path = os.path.join(cls.data_folder, file_name + ".json")
+        with open(file_path, "w", encoding="utf-8") as json_file:
             json.dump(data, json_file, ensure_ascii=False, indent=4)
 
     def __init__(
@@ -44,7 +59,7 @@ class Tournois:
         self.infos_turnament = infos_turnament
 
     def add_turnament(self):
-        """Ajouter un nouveau tournois"""
+        """Ajouter un nouveau tournois ou returne une expetion ValueError"""
         actual_round = 0
         turnois = {
             "Nom": self.name,
@@ -57,24 +72,12 @@ class Tournois:
             "Informations turnois": self.infos_turnament,
         }
 
-        data = Tournois._get_json_file()
-
-        # Vérification des doublons
-        if any(
-            re.sub(r"[^a-zA-Z0-9]", "", el.get("Nom").lower())
-            == re.sub(r"[^a-zA-Z0-9]", "", turnois["Nom"].lower())
-            for el in data
-        ):
-            print("Turnois déjà existant")
-        else:
-            data.append(turnois)
-
-        # Écrire les données dans le fichier JSON
-        Tournois._write_json_file(data)
+        data_file_name = re.sub(r"[^a-zA-Z0-9]", "", self.name)
+        Tournois._create_json_file(data_file_name, turnois)
 
     def get_turnament(self, turnament_name):
-        """Récupérer les informations d'un tournois"""
-        data = Tournois._get_json_file()
+        """Récupérer les informations d'un tournois, returne le turnois ou KeyError exception"""
+        data = Tournois._get_json_file(turnament_name)
 
         for el in data:
             # Parcourir la liste des éléments et vérifier si l'ID correspond
@@ -82,10 +85,12 @@ class Tournois:
                 r"[^a-zA-Z0-9]", "", turnament_name.lower()
             ):
                 return el
+            else:
+                raise KeyError("Ce Tournois n'est pas enregistré")
 
     def update_turnament(self, turnament_name, turnament_data):
-        """Mettre à jour les informations d'un joueur"""
-        data = Tournois._get_json_file()
+        """Mettre à jour les informations d'un joueur ou returne une exception KeyError"""
+        data = Tournois._get_json_file(turnament_name)
 
         for i, element in enumerate(data):
             # Parcourir la liste des éléments et vérifier si l'ID correspond
@@ -95,13 +100,15 @@ class Tournois:
                 data.append(turnament_data)
                 # Ajouter l'élément mis à jour
                 break
+            else:
+                raise KeyError("Ce Tournois n'est pas enregistré")
 
         # Écrire les données dans le fichier JSON
         Tournois._write_json_file(data)
 
 
 my_tournament = Tournois(
-    name="tournois1",
+    name="Paris 2023",
     place="Paris",
     start_date="2023-05-01",
     end_date="2023-05-08",
@@ -120,31 +127,32 @@ my_tournament = Tournois(
         "Nulla": "Nulla eu tellus efficitur.",
     },
 )
+my_tournament.add_turnament()
 # my_tournament.add_turnament()
 
 # print(my_tournament.get_turnament("tournois1"))
 
-my_tournament.update_turnament(
-    "tournois1",
-    {
-        "Nom": "tournois1",
-        "Lieu": "Paris",
-        "Date debut": "2023-05-01",
-        "Date fin": "2023-05-08",
-        "Tours depart": 6,
-        "Tour Actuel": 1,
-        "Joueurs": ["toto", "tata"],
-        "Informations turnois": {
-            "Lorem": "Lorem ipsum dolor sit amet.",
-            "Integer": "Integer accumsan dui.",
-            "Pellentesque": "Pellentesque eget enim ut nibh.",
-            "Vestibulum": "Vestibulum sagittis tellus.",
-            "Fusce": "Fusce eu lectus id ante.",
-            "Cras": "Cras semper enim a ex venenatis.",
-            "Aliquam": "Aliquam erat volutpat.",
-            "Maecenas": "Maecenas auctor turpis.",
-            "Morbi": "Morbi euismod nisl.",
-            "Nulla": "Nulla eu tellus efficitur.",
-        },
-    },
-)
+# my_tournament.update_turnament(
+#     "tournois1",
+#     {
+#         "Nom": "tournois1",
+#         "Lieu": "Paris",
+#         "Date debut": "2023-05-01",
+#         "Date fin": "2023-05-08",
+#         "Tours depart": 6,
+#         "Tour Actuel": 1,
+#         "Joueurs": ["toto", "tata"],
+#         "Informations turnois": {
+#             "Lorem": "Lorem ipsum dolor sit amet.",
+#             "Integer": "Integer accumsan dui.",
+#             "Pellentesque": "Pellentesque eget enim ut nibh.",
+#             "Vestibulum": "Vestibulum sagittis tellus.",
+#             "Fusce": "Fusce eu lectus id ante.",
+#             "Cras": "Cras semper enim a ex venenatis.",
+#             "Aliquam": "Aliquam erat volutpat.",
+#             "Maecenas": "Maecenas auctor turpis.",
+#             "Morbi": "Morbi euismod nisl.",
+#             "Nulla": "Nulla eu tellus efficitur.",
+#         },
+#     },
+# )
